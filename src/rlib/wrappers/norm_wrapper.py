d5f9f90ba131034a27ae55377f12cb59ba76561d
axeldinh <axeldvc@gmail.com>
2023-05-14 15:03:56 +0200

    Remove warning by copying values

--- src/rlib/wrappers/norm_wrapper.py
+++ src/rlib/wrappers/norm_wrapper.py
@@ -1,4 +1,5 @@
 
+import gymnasium
 from gymnasium import ObservationWrapper
 
 class NormWrapper(ObservationWrapper):
@@ -9,17 +10,37 @@ class NormWrapper(ObservationWrapper):
 
     def __init__(self, env):
         super().__init__(env)
-        self.min_values = env.observation_space.low
-        self.max_values = env.observation_space.high
+        self.min_values = env.observation_space.low.copy()
+        self.max_values = env.observation_space.high.copy()
 
         normalized_states = (self.min_values >= -1e10) * (self.max_values <= 1e10)
 
-        self.observation_space.low[normalized_states] = -1
-        self.observation_space.high[normalized_states] = 1
+        low = self.min_values.copy()
+        low[normalized_states] = -1
+        high = self.max_values.copy()
+        high[normalized_states] = 1
 
-        self.min_values[self.min_values <= -1e10] = 0
-        self.max_values[self.max_values >= 1e10] = 1
+        self.observation_space = gymnasium.spaces.Box(
+                low=low, 
+                high=high,
+                shape=self.min_values.shape, dtype=low.dtype)
+
+        self.min_values[~normalized_states] = 0
+        self.max_values[~normalized_states] = 1
         
 
     def observation(self, observation):
-        return (observation - self.min_values) / (self.max_values - self.min_values) * 2 - 1
+        observation = (observation - self.min_values) / (self.max_values - self.min_values) * 2 - 1
+        return observation
+
+if __name__ == "__main__":
+
+    env = gymnasium.make("CartPole-v1")
+    env = NormWrapper(env)
+
+    print(env.env.observation_space)
+    obs, _ = env.reset()
+    action = env.action_space.sample()
+    print(obs)
+    obs, reward, done, _, _ = env.step(action)
+    print(obs)
