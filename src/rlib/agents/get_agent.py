a4c4b3e2607b89737b84f5acb202474fc36faff0
axeldinh <axeldvc@gmail.com>
2023-08-02 20:12:16 +0200

    doc

--- src/rlib/agents/get_agent.py
+++ src/rlib/agents/get_agent.py
@@ -2,6 +2,17 @@ from .q_table import QTable
 from .mlp import MLP
 
 def get_agent(agent_type, **kwargs):
+    """ Global function to get an agent from its type and parameters
+
+    This is the function used in the algorithms when a kwargs for an agent are given.
+    The agent_type is usually automatically inferred from the environment's observation and ction spaces.
+
+    :param agent_type: type of the agent, either "mlp" or "q_table", "cnn" is not supported yet
+    :type agent_type: str
+
+    :return: agent
+    
+    """
 
     if agent_type == "mlp":
         return MLP(**kwargs)
