19a06422117c7e289c02a3d6c4242e9d6eebd454
axeldinh <axeldvc@gmail.com>
2023-09-02 15:33:13 +0200

    separate the setup.py from the code
    brings import errors

--- setup.py
+++ setup.py
@@ -2,9 +2,32 @@
 import pathlib
 from setuptools import setup
 
-from src.rlib.utils import get_git_infos
+import subprocess
+import os
 
-get_git_infos()
+path = os.path.dirname(os.path.abspath(__file__))
+
+# Get the remote url
+remote_url = subprocess.check_output(["git", "config", "--get", "remote.origin.url"], 
+                                        cwd=path).decode("utf-8").strip()
+
+# Get the commit hash
+commit_hash = subprocess.check_output(["git", "rev-parse", "HEAD"], 
+                                        cwd=path).decode("utf-8").strip()
+
+# Get the commit message
+commit_message = subprocess.check_output(["git", "log", "-1", "--pretty=%B"],
+                                            cwd=path).decode("utf-8").strip().replace("|", " ").replace("\n", " ")
+
+# Get the branch name
+branch_name = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"],
+                                        cwd=path).decode("utf-8").strip()
+
+with open(os.path.abspath("src/rlib/__git_infos__.py"), "w") as infos_file:
+    infos_file.write(f'__remote_url__ = "{remote_url}"\n')
+    infos_file.write(f'__commit_hash__ = "{commit_hash}"\n')
+    infos_file.write(f'__commit_message__ = "{commit_message}"\n')
+    infos_file.write(f'__branch_name__ = "{branch_name}"\n')
 
 def get_requirements():
     with open(pathlib.Path(__file__).parent.resolve().joinpath("requirements.txt")) as requirements_file:
